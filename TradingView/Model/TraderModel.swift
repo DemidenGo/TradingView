1774a64c1c660a7601dd6dd03e90edc6fa0456e7
Demidengo <dimidengo@yandex.ru>
2023-10-18 00:08:44 +0300

    Mock trader model completed

---
+++ TradingView/Model/TraderModel.swift
@@ -0,0 +1,65 @@
+//
+//  TraderModel.swift
+//  TradingView
+//
+//  Created by Юрий Демиденко on 17.10.2023.
+//
+
+import Foundation
+
+enum Country {
+    case usa, can, bra, jap, ger, fra, aus, ind, spa
+}
+
+struct TraderModel {
+
+    let name: String
+    let country: Country
+    let deposit: Int
+    let profit: Int
+
+    static func makeMockModel() -> [TraderModel] {
+        var model = [TraderModel]()
+        model.append(TraderModel(name: "Oliver",
+                                 country: .usa,
+                                 deposit: 8367,
+                                 profit: 336755))
+        model.append(TraderModel(name: "Jack",
+                                 country: .can,
+                                 deposit: 7489,
+                                 profit: 287190))
+        model.append(TraderModel(name: "Harry",
+                                 country: .bra,
+                                 deposit: 7004,
+                                 profit: 230712))
+        model.append(TraderModel(name: "Jacob",
+                                 country: .jap,
+                                 deposit: 6489,
+                                 profit: 198640))
+        model.append(TraderModel(name: "Charley",
+                                 country: .ger,
+                                 deposit: 6084,
+                                 profit: 167500))
+        model.append(TraderModel(name: "Thomas",
+                                 country: .bra,
+                                 deposit: 5602,
+                                 profit: 139750))
+        model.append(TraderModel(name: "George",
+                                 country: .fra,
+                                 deposit: 4992,
+                                 profit: 97600))
+        model.append(TraderModel(name: "Oscar",
+                                 country: .aus,
+                                 deposit: 4299,
+                                 profit: 75100))
+        model.append(TraderModel(name: "James",
+                                 country: .ind,
+                                 deposit: 3015,
+                                 profit: 53760))
+        model.append(TraderModel(name: "William",
+                                 country: .spa,
+                                 deposit: 1563,
+                                 profit: 25905))
+        return model
+    }
+}
